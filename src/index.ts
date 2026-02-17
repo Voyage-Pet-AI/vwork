808d643d966f4e58f6d02e19974051c79684f30e
Ricardo Ma <ricardo@jarvisprd.com>
2026-02-16 21:58:21 -0800

    v0.1.4

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ export function createProvider(config: Config): LLMProvider {
   }
 }
 
-const VERSION = "0.1.3";
+const VERSION = "0.1.4";
 
 const args = process.argv.slice(2);
 const command = args[0] ?? "chat";
